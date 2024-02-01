e5f8c8b4ba17c870e4045252b00d29db04496143
Jun Park <90596660+CA-JunPark@users.noreply.github.com>
2024-02-01 14:49:35 -0800

    ad

--- ContestAdvancement.py
+++ ContestAdvancement.py
@@ -1,21 +1,33 @@
 n, k, c = map(int, input().split()) # total, advance, limit
 
-num = {i: [] for i in range(1, n + 1)} #key = school, value = id(s)
+advance = {i: [] for i in range(1, n + 1)} #key = school, value = id(s)
+remaining = []
 result = []
 counter = 0
 for i in range(n):
     id, school = map(int, input().split())
     if counter < k:
-        if len(num[school]) < c:
-            num[school].append(id)
-            result.append(id)
+        if len(advance[school]) < c:
+            advance[school].append(id)
+            result.append((id, school))
             counter += 1
-        
-for j in result:
-    print(j)
-    
-    
-# 10 7 3
+        else:
+            remaining.append((id, school))
+
+need = k - len(result) 
+current = result[0][1]
+for j in range(len(result)):
+    print(result[j][0])
+    if j < len(result)-1:
+        if result[j][1] != result[j+1][1]:
+            current = result[j+1][1]
+            if need > 0:
+                print(remaining[0][0])
+                remaining.pop(0)
+                need -=1
+
+
+# 10 7 2
 # 3 9
 # 1 9
 # 4 9
