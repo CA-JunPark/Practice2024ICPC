db08c5d1efa81c518dd57dd49d11a639075a38e6
Jun Park <90596660+CA-JunPark@users.noreply.github.com>
2024-02-01 14:19:06 -0800

    ad

--- ContestAdvancement.py
+++ ContestAdvancement.py
@@ -5,12 +5,24 @@ result = []
 counter = 0
 for i in range(n):
     id, school = map(int, input().split())
-    if len(num[school]) < c:
-        num[school].append(id)
-        result.append(id)
-        counter += 1
-        if counter == k:
-            break;
-
+    if counter < k:
+        if len(num[school]) < c:
+            num[school].append(id)
+            result.append(id)
+            counter += 1
+        
 for j in result:
     print(j)
+    
+    
+# 10 7 3
+# 3 9
+# 1 9
+# 4 9
+# 5 9
+# 9 7
+# 2 7
+# 6 7
+# 7 7
+# 8 5
+# 10 5
